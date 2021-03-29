e5efaf754e9f5aea9283786a4586a2394a46fcf8
Christophe Damas <christophe.damas.r@gmail.com>
2021-03-29 11:50:30 +0200

    29.03.2021

--- FINAL/js/script.js
+++ FINAL/js/script.js
@@ -8,5 +8,77 @@ $(function () {
         $.getScript("js/table.js", function () { });
     });
 
+    $('#modal2').modal();
+    $('#delModal').modal();
+
+    $('.save').click(function () {
+        console.log("save")
+        var id = $('#id').val();
+        // console.log(id);
+        // console.log('click submit');
+        $.ajax({
+            type: "POST",
+            url: "api.php?id=" + id,
+            data: {
+                name: $('#name').val(),
+                kraftstoff: $('#kraftstoff').val(),
+                color: $('#color').val(),
+                bauart: $('#bauart').val(),
+                tank: $('#tank').val()
+            },
+            dataType: "JSON",
+            success: function (response) {
+                console.log('post success');
+                console.log(response);
+                console.log(data);
+            }
+        })
+        M.toast({html: modus + " - Saved", classes: 'green white-text'})
+        getData();
+    });
+    
+    $('.cancel').click(function () {
+        console.log("cancel")
+        M.toast({html: modus + " - Canceled", classes: 'red white-text'})
+    })
+
+    
+
+    $('.deleteToast').click(function () {
+        console.log("deleted")
+        $.ajax({
+            type: "DELETE",
+            url: "api.php?id=" + id,
+            dataType: "JSON",
+            success: function (response) {
+                console.log("delete success");
+                getData();
+            }
+        });
+        M.toast({html: "Successfull Deleted", classes: 'green white-text'})
+    })
     
 });
+
+var id;
+var modus;
+
+function showDelModal(id) {
+    this.id = id;
+    // console.log(id);
+    $('#modal-main').load("sites/delModal.html", function () {
+        $('#modal-title').html("Delete");
+        $('#elementID').html(" '" + id + "' ");
+    });
+
+}
+
+function showModal(id, modus) {
+    this.id = id;
+    this.modus = modus;
+    
+    $('#modal-content').load("sites/formular.html", function () {
+        $.getScript("js/formular.js")
+    });
+
+}
